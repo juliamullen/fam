352b3998d75b9e232064e609469c0beb188c1f7d
tim <timothy.liam.mullen@gmail.com>
2015-12-20 23:09:57 -0500

    views & urls

--- fam/user/views.py
+++ fam/user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from user.models import
 
-# Create your views here.
+def feed(request):
+    return render(request, 'user/feed.html', {})
+
+def event_view(request):
+    pass
+
+def profile_view(request):
+    pass
